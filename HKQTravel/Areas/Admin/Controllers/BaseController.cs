973b7b7a946f740218c93a323c20a388d7ecc469
halamhung <hungdeptrai111@gmail.com>
2023-06-03 03:42:41 +0700

    first commit

---
+++ HKQTravel/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HKQTravel.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Admin/Base
+        public BaseController()
+        {
+            //tạo ra 1 hàm constructer bắt mọi trang admin đều phải đăng nhập
+            if (System.Web.HttpContext.Current.Session["admin_user"].Equals(""))
+            {
+                //return redirect("~/admin/login"); lớp này không thể trả về bằng cách này 
+                System.Web.HttpContext.Current.Response.Redirect("~/admin/login");
+            }
+        }
+    }
+}
