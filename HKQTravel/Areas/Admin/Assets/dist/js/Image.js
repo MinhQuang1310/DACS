8866a79c7f7470d94dfc0deab1f86a205a6565d1
halamhung <hungdeptrai111@gmail.com>
2023-06-04 13:53:16 +0700

    4/6/2023

--- HKQTravel/Areas/Admin/Assets/dist/js/Image.js
+++ HKQTravel/Areas/Admin/Assets/dist/js/Image.js
@@ -1,15 +1,29 @@
-﻿function previewImage() {
-    var preview = document.querySelector('#imgPreview');
-    var file = document.querySelector('#fileUpLoad').files[0];
-    var reader = new FileReader();
+﻿//function previewImage() {
+//    var preview = document.querySelector('#imgPreview');
+//    var file = document.querySelector('#fileUpLoad').files[0];
+//    var reader = new FileReader();
 
-    reader.onloadend = function () {
-        preview.src = reader.result;
-    }
+//    reader.onloadend = function () {
+//        preview.src = reader.result;
+//    }
+
+//    if (file) {
+//        reader.readAsDataURL(file);
+//    } else {
+//        preview.src = "";
+//    }
+//}
+
+fileImage.onchange = evt => {
+
+    const [file] = fileImage.files
 
     if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        preview.src = "";
+
+        preview.src = URL.createObjectURL(file);
+
+        $("#preview").removeClass("hidden");
+
     }
+
 }
